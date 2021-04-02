b3d10fb9d0d0bfc1d4d797f995318ba6e7f89c62
lianyongsheng <lianys2021@163.com>
2021-04-02 10:05:01 +0800

    rebase

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'lian-hooks'
+import 'lian-hooks/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
